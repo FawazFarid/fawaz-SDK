299d6ef19ae7b2eb5ce6833615d6e8d228895b44
Fawaz Farid <fawwazally@gmail.com>
2023-02-16 10:06:21 +0300

    Add pagination and filtering options

--- lib/lotr/client.rb
+++ lib/lotr/client.rb
@@ -14,8 +14,8 @@ module Lotr
       Lotr::VERSION
     end
 
-    def movies
-      resp = self.class.get("/movie", headers: @headers)
+    def movies(options = {})
+      resp = self.class.get("/movie", headers: @headers, query: parse_query_params(options))
       handle_response(resp)
     end
 
@@ -24,8 +24,8 @@ module Lotr
       handle_response(resp)
     end
 
-    def quotes_for_movie(movie_id)
-      resp = self.class.get("/movie/#{movie_id}/quote", headers: @headers)
+    def quotes_for_movie(movie_id, options = {})
+      resp = self.class.get("/movie/#{movie_id}/quote", headers: @headers, query: parse_query_params(options))
       handle_response(resp)
     end
 
@@ -39,6 +39,13 @@ module Lotr
 
     def handle_error_response(resp)
       raise Lotr::Error, "Error: #{resp.code} #{resp.message}"
+    end
+
+    def parse_query_params(options)
+      # merge the sort and order params into one e.g sort=name:asc
+      options[:sort] = "#{options[:sort]}:#{options[:order]}" if options[:order]
+      options.delete(:order)
+      options
     end
   end
 end
--- spec/lotr/client_spec.rb
+++ spec/lotr/client_spec.rb
@@ -7,17 +7,31 @@ RSpec.describe Lotr::Client do
     @client = Lotr::Client.new(api_key: test_lotr_api_key)
   end
 
+  let(:movie_id) do
+    "5cd95395de30eff6ebccde5c"
+  end
+
   describe ".movies", :vcr do
     it "returns all movies" do
       resp = @client.movies
       expect(resp["docs"]).to be_kind_of Array
       expect(resp["docs"].first["name"]).to be_kind_of String
     end
+
+    it "returns movies with a limit" do
+      resp = @client.movies(limit: 1)
+      expect(resp["docs"].length).to eq 1
+    end
+
+    it "returns movies with a sort" do
+      resp = @client.movies(sort: "name", order: "asc")
+      expect(resp["docs"].first["name"]).to eq "The Battle of the Five Armies"
+    end
   end
 
   describe ".movie", :vcr do
     it "returns a movie" do
-      resp = @client.movie("5cd95395de30eff6ebccde5c")
+      resp = @client.movie(movie_id)
       expect(resp["docs"]).to be_kind_of Array
       expect(resp["docs"].first["name"]).to be_kind_of String
     end
@@ -25,9 +39,19 @@ RSpec.describe Lotr::Client do
 
   describe ".quotes_for_movie", :vcr do
     it "returns quotes for a movie" do
-      resp = @client.quotes_for_movie("5cd95395de30eff6ebccde5c")
+      resp = @client.quotes_for_movie(movie_id)
       expect(resp["docs"]).to be_kind_of Array
       expect(resp["docs"].first["dialog"]).to be_kind_of String
+    end
+
+    it "returns quotes for a movie with a limit" do
+      resp = @client.quotes_for_movie(movie_id, limit: 1)
+      expect(resp["docs"].length).to eq 1
+    end
+
+    it "returns quotes for a movie with a sort" do
+      resp = @client.quotes_for_movie(movie_id, sort: "dialog", order: "desc")
+      expect(resp["docs"].first["dialog"]).to eq "hmmmhmmm"
     end
   end
 end
